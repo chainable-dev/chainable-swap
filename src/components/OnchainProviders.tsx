99da048becee080b08e155e9ab77801091722a3a
SourC <souriya.khaosanga@gmail.com>
2024-11-10 01:57:06 -0800

    fix

--- src/components/OnchainProviders.tsx
+++ src/components/OnchainProviders.tsx
@@ -20,7 +20,20 @@ function OnchainProviders({ children }: Props) {
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={base}>
+<<<<<<< Updated upstream
           <RainbowKitProvider modalSize="compact">
+=======
+          <RainbowKitProvider
+            coolMode
+            theme={darkTheme({
+              accentColor: '#3EB8B3',
+              accentColorForeground: 'white',
+              borderRadius: 'large',
+              overlayBlur: 'small',
+              fontStack: 'system',
+            })}
+          >
+>>>>>>> Stashed changes
             {children}
           </RainbowKitProvider>
         </OnchainKitProvider>
---
+++ src/types/custom.d.ts
@@ -0,0 +1,4 @@
+// types/custom.d.ts
+declare module '@coinbase/onchainkit/node_modules/viem' {
+    export * from 'viem';
+}
